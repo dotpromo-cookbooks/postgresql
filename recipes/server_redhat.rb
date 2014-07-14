c579e313076569ff959559ac2c57ff79406085bd
Alexander Simonov <alex@simonov.me>
2014-07-14 19:07:58 +0300

    Fix redhat platform. Add support for systemd on newest fedora, rhel and centos

--- recipes/server_redhat.rb
+++ recipes/server_redhat.rb
@@ -17,72 +17,36 @@
 
 include_recipe "postgresql::client"
 
+::Chef::Recipe.send(:include, Opscode::PostgresqlHelpers)
 svc_name = node['postgresql']['server']['service_name']
 dir = node['postgresql']['dir']
 initdb_locale = node['postgresql']['initdb_locale']
+version = node['postgresql']['version']
 
-# Create a group and user like the package will.
-# Otherwise the templates fail.
-
-group "postgres" do
-  gid 26
-end
-
-user "postgres" do
-  shell "/bin/bash"
-  comment "PostgreSQL Server"
-  home "/var/lib/pgsql"
-  gid "postgres"
-  system true
-  uid 26
-  supports :manage_home => false
-end
-
-directory dir do
-  owner "postgres"
-  group "postgres"
-  recursive true
-  action :create
-end
-
-node['postgresql']['server']['packages'].each do |pg_pack|
-
-  package pg_pack
-
-end
-
-# Starting with Fedora 16, the pgsql sysconfig files are no longer used.
-# The systemd unit file does not support 'initdb' or 'upgrade' actions.
-# Use the postgresql-setup script instead.
-
-unless platform_family?("fedora") and node['platform_version'].to_i >= 16
-
-  directory "/etc/sysconfig/pgsql" do
-    mode "0644"
-    recursive true
-    action :create
+if systemd?
+  unless data_dir == "/var/lib/pgsql/#{version}/data"
+    template "/etc/systemd/system/postgresql-#{version}.service" do
+      source "postgresql.service.erb"
+      mode "0644"
+    end
   end
-
+else
   template "/etc/sysconfig/pgsql/#{svc_name}" do
     source "pgsql.sysconfig.erb"
     mode "0644"
     notifies :restart, "service[postgresql]", :delayed
   end
-
 end
 
-if platform_family?("fedora") and node['platform_version'].to_i >= 16
-
-  execute "postgresql-setup initdb #{svc_name}" do
+unless platform_family?("suse")
+  setup_command = if systemd?
+                    systemd_initdb_cmd
+                  else
+                    sysinit_initdb_cmd
+                  end
+  execute setup_command do
     not_if { ::FileTest.exist?(File.join(dir, "PG_VERSION")) }
   end
-
-else !platform_family?("suse") 
-
-  execute "/sbin/service #{svc_name} initdb #{initdb_locale}" do
-    not_if { ::FileTest.exist?(File.join(dir, "PG_VERSION")) }
-  end
-
 end
 
 include_recipe "postgresql::server_conf"
